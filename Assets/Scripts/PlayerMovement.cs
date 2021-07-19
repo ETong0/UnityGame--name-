6a19c7af02071381d5bb189ce86755525973c6b6
ETong0 <ETong.2023@trinity.nsw.edu.au>
2021-07-19 20:38:06 +1000

    practice game start

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+	{
+	private Rigidbody2D body;
+
+	private void Awake()
+	{
+		body = GetComponent<Rigidbody2D>();
+	}
+
+    private void Update()
+    {
+		body.velocity = new Vector2(Input.GetAxis("Horizontal"), body.velocity.y);
+    }
+
+}
