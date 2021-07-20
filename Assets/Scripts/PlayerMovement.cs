7a8706b085ddb7394bd3f2b5eda4fb9beddf64fb
ETong0 <ETong.2023@trinity.nsw.edu.au>
2021-07-20 22:48:18 +1000

    adding run animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,12 @@ public class PlayerMovement : MonoBehaviour
 
 	[SerializeField]private float speed;
 	private Rigidbody2D body;
+	private Animator anim;
 
 	private void Awake()
 	{
 		body = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
 	}
 
     private void Update()
@@ -21,12 +23,15 @@ public class PlayerMovement : MonoBehaviour
 		//flip sprite when facing left and right
 		if (horizontalInupt > 0.01f)
 			transform.localScale = Vector3.one;
-		else if (horizontalInupt > -0.01f)
+		else if (horizontalInupt < -0.01f)
 			transform.localScale = new Vector3(-1,1,1);
 
 		//jump
 		if (Input.GetKey(KeyCode.Space))
 			body.velocity = new Vector2(body.velocity.x, speed);
+
+		//Set animator peramitor
+		anim.SetBool("run", horizontalInupt != 0);
     }
 
 }
