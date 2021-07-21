af98958907ae6b8813d91b78ed98e62b6072842c
ETong0 <ETong.2023@trinity.nsw.edu.au>
2021-07-21 22:51:02 +1000

    adding attack animaiton

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -58,4 +58,9 @@ public class PlayerMovement : MonoBehaviour
 		RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center,boxCollider.bounds.size,0, Vector2.down, 0.1f, groundLayer);
 		return raycastHit.collider!=null;
     }
+
+	public bool canAttack()
+    {
+		return true;
+    }
 }
