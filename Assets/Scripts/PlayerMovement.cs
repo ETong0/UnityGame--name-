f4612f132e642ecf1dcbaade6c460180138b60fe
ETong0 <ETong.2023@trinity.nsw.edu.au>
2021-07-21 22:11:27 +1000

    add box collisder for jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,17 @@ public class PlayerMovement : MonoBehaviour
 	{
 
 	[SerializeField]private float speed;
+	[SerializeField] private LayerMask groundLayer;
 	private Rigidbody2D body;
 	private Animator anim;
-	private bool grounded;
+	private BoxCollider2D boxCollider;
+
 
 	private void Awake()
 	{
 		body = GetComponent<Rigidbody2D>();
 		anim = GetComponent<Animator>();
+		boxCollider = GetComponent<BoxCollider2D>();
 	}
 
     private void Update()
@@ -28,27 +31,31 @@ public class PlayerMovement : MonoBehaviour
 			transform.localScale = new Vector3(-1,1,1);
 
 		//jump
-		if (Input.GetKey(KeyCode.Space) && grounded)
+		if (Input.GetKey(KeyCode.Space) && isGrounded())
 			Jump();
 
 		//Set animator peramitor
 		anim.SetBool("run", horizontalInupt != 0);
-		anim.SetBool("grounded", grounded);
+		anim.SetBool("grounded", isGrounded());
     }
 
 	//Jump function
 	private void Jump()
     {
-		body.velocity = new Vector2(body.velocity.x, speed);
+		body.velocity = new Vector2(body.velocity.x, speed*2);
 		anim.SetTrigger("jump");
-		grounded = false;
     }
 
     //check for collision
     private void OnCollisionEnter2D(Collision2D collision)
     {
-		if (collision.gameObject.tag == "Ground")
-			grounded = true;
+		
     }
 
+
+	private bool isGrounded()
+    {
+		RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center,boxCollider.bounds.size,0, Vector2.down, 0.1f, groundLayer);
+		return raycastHit.collider!=null;
+    }
 }
