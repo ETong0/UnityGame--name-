67404b1d96231aedbf6c2533cc3715ee89be6575
ETong0 <ETong.2023@trinity.nsw.edu.au>
2021-07-29 09:54:08 +1000

    debug, make character stop and slid down wall

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 
 	[SerializeField]private float speed;
 	[SerializeField] private LayerMask groundLayer;
+	[SerializeField] private LayerMask wallLayer;
 	private Rigidbody2D body;
 	private Animator anim;
 	private BoxCollider2D boxCollider;
@@ -18,26 +19,31 @@ public class PlayerMovement : MonoBehaviour
 		boxCollider = GetComponent<BoxCollider2D>();
 	}
 
-    private void Update()
-    {
+	private void Update()
+	{
 		//move left and right
+
 		float horizontalInupt = Input.GetAxis("Horizontal");
-		body.velocity = new Vector2(horizontalInupt * speed, body.velocity.y);
+
+		if (!isWall())
+			body.velocity = new Vector2(horizontalInupt * speed, body.velocity.y);
 
 		//flip sprite when facing left and right
 		if (horizontalInupt > 0.01f)
 			transform.localScale = Vector3.one;
+		
 		else if (horizontalInupt < -0.01f)
-			transform.localScale = new Vector3(-1,1,1);
+			transform.localScale = new Vector3(-1, 1, 1);
 
 		//jump
-		if (Input.GetKey(KeyCode.Space) && isGrounded())
+		if (Input.GetKey(KeyCode.UpArrow) && isGrounded())
+		{
 			Jump();
-
+		}
 		//Set animator peramitor
 		anim.SetBool("run", horizontalInupt != 0);
 		anim.SetBool("grounded", isGrounded());
-    }
+	}
 
 	//Jump function
 	private void Jump()
@@ -58,6 +64,13 @@ public class PlayerMovement : MonoBehaviour
 		RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center,boxCollider.bounds.size,0, Vector2.down, 0.1f, groundLayer);
 		return raycastHit.collider!=null;
     }
+
+	private bool isWall()
+	{
+		RaycastHit2D HitLeft = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.left, 0.1f, wallLayer);
+		RaycastHit2D HitRight = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.right, 0.1f, wallLayer);
+		return HitLeft.collider != null || HitRight.collider != null;
+	}
 
 	public bool canAttack()
     {
