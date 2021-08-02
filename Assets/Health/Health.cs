856158e02755a290648fbedcf0b715c3d6c13af0
ETong0 <ETong.2023@trinity.nsw.edu.au>
2021-08-02 22:34:24 +1000

    adding health system

---
+++ Assets/Health/Health.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    private float currentHealth;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+
+    }
+
+    public void TackDanmage(float _damage)
+    {
+
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+
+        if (currentHealth > 0)
+        {
+            //Player Hurt
+        }
+        else
+        {
+            //Player dead
+        }
+
+    }
+
+
+}
