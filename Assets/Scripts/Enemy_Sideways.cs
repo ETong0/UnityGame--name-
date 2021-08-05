291807e35dc97a7ad5766790ba30fb471228f150
ETong0 <ETong.2023@trinity.nsw.edu.au>
2021-08-05 21:23:09 +1000

    adding saw code

---
+++ Assets/Scripts/Enemy_Sideways.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class Enemy_Sideways : MonoBehaviour
+{
+
+    [SerializeField] private float damange;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.GetComponent<Health>().TakeDanmage(damange);
+        }
+    }
+}
