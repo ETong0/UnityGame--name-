0a708f9aad944d51320dbb6e5b3b6273dc67454f
ETong0 <ETong.2023@trinity.nsw.edu.au>
2021-08-09 21:46:53 +1000

    Update Health.cs

--- Assets/Health/Health.cs
+++ Assets/Health/Health.cs
@@ -2,15 +2,23 @@
 
 public class Health : MonoBehaviour
 {
+
+    [Header("Health")]
     [SerializeField] public float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
 
+    [Header("iFrame")]
+    [SerializeField]private float iFrameDuration;
+    [SerializeField] private float numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
     public void TakeDanmage(float _damage)
     {
@@ -20,6 +28,7 @@ public class Health : MonoBehaviour
         {
             anim.SetTrigger("hurt");
             //iframes
+            StartCoroutine(Invonerability());
         }
         else
         {
@@ -39,6 +48,17 @@ public class Health : MonoBehaviour
     public void addHealth(float _value)
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+    private System.Collections.IEnumerator Invonerability()
+    {
+        Physics2D.IgnoreLayerCollision(10, 12, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            
+        }
+        Physics2D.IgnoreLayerCollision(10, 12, false);
+
     }
 
 }
