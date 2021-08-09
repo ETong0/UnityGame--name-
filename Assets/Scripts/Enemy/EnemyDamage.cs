8aeb1f56ca943c8f3f3d507528b7321c5054934a
ETong0 <ETong.2023@trinity.nsw.edu.au>
2021-08-09 22:18:00 +1000

    add Take damange function

--- Assets/Scripts/Enemy/EnemyDamage.cs
+++ Assets/Scripts/Enemy/EnemyDamage.cs
@@ -5,10 +5,11 @@ public class EnemyDamage : MonoBehaviour
 {
     [SerializeField]private float damage;
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.tag == "player")
-            
-    }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "player")
+            collision.GetComponent<Health>().TakeDanmage(damage);
+    }
+   
 }
