9aecede5b9f8796dc53152da001d7cd954b6e532
ETong0 <ETong.2023@trinity.nsw.edu.au>
2021-08-09 22:12:50 +1000

    clean up

---
+++ Assets/Scripts/Enemy/EnemyDamage.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField]private float damage;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.tag == "player")
+            
+    }
+
+}
